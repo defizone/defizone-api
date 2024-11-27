0cd39ef87a292fc027e88e20e0cb6535932ae6d1
alice <dev@defizone.pro>
2024-11-27 23:59:30 +1000

    Created model License

--- internal/model/model.go
+++ internal/model/model.go
@@ -1,1 +1,14 @@
 package model
+
+import "time"
+
+type License struct {
+	Key         string
+	Token       string
+	OwnerId     string
+	DeviceId    string
+	ExpiredDate time.Time
+	Duration    int
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+}
