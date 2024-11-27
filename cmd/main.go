c698e71762666290dde455cf791580845e69584b
alice <dev@defizone.pro>
2024-11-28 00:07:04 +1000

    Created model License

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+)
 
+func getIndex(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World"))
+}
+
+func main() {
+	router := http.NewServeMux()
+
+	router.HandleFunc("/", getIndex)
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+
+	log.Println("Starting server on port 8080")
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
 }
